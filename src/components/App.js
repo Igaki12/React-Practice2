6f40d828c44d9031e8cd66ae73c99f2a587f79a8
unknown <igatatsu1997@gmail.com>
2022-01-24 17:23:22 +0900

    Wrote todos.js & useTodo.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,4 @@
-import React,{useState, useEffect} from "react";
-import axios from "axios";
-const todoDataUrl = "http://localhost:3100/todos";
+import {useTodo} from "../hooks/useTodo";
 const TodoTitle = ({title,as}) => {
   if(as === "h1") return <h1>{title}</h1>
   if(as === "h2") return <h2>{title}</h2>
@@ -25,14 +23,7 @@ const TodoList = ({todoList}) => {
   )
 }
 function App(){
-  const [todoList,setTodoList] = useState([])
-  useEffect(()=>{
-    const fetchData = async () => {
-      const response = await axios.get(todoDataUrl);
-      setTodoList(response.data);
-    };
-    fetchData();
-  },[])
+  const {todoList} = useTodo();
   console.log("TODOリスト:",todoList);
   const inCompletedList = todoList.filter((todo)=>{
     return !todo.done;
