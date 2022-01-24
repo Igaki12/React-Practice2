b4d4d21738d75257ca346b4ace686aff137097bd
unknown <igatatsu1997@gmail.com>
2022-01-24 09:23:10 +0900

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React,{useState, useEffect} from "react";
+import axios from "axios";
+const todoDataUrl = "http://localhost:3100/todos";
+function App(){
+  const [todoList,setTodoList] = useState([])
+  useEffect(()=>{
+    const fetchData = async () => {
+      const response = await axios.get(todoDataUrl);
+      setTodoList(response.data);
+    };
+    fetchData();
+  },[])
+  console.log("TODOリスト:",todoList);
+  return (
+    <>
+      <h1>TODO進捗管理</h1>
+      <textarea />
+      <button>TODOを追加</button>
+      <h2>TODOリスト</h2>
+      <ul>
+        {todoList.map((todo) => {
+          <li key={todo.id}>
+            {todo.content}({todo.done ? "完了":"未完"})
+          </li>
+
+        })}
+      </ul>
+    </>
+  );
+}
+export default App;
